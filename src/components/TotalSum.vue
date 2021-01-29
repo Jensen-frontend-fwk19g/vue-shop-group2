ad69bfbe5392666ecae376650d3e36e202561ab2
jensengbg-sanel-jasarevic <55239936+jensengbg-sanel-jasarevic@users.noreply.github.com>
2021-01-29 23:30:42 +0100

    Started working on test cases

--- src/components/TotalSum.vue
+++ src/components/TotalSum.vue
@@ -1,13 +1,17 @@
 <template>
-
-
+        <h1>Total sum
+        </h1>
 </template>
 
 <script>
 export default {
     name: "TotalSum",
+    
+    data: () => ({
+        totalSum: 0,
+        cartOrders: []   
+         }),
 
-  
 }
 </script>
 
--- tests/unit/TotalSum.spec.js
+++ tests/unit/TotalSum.spec.js
@@ -1,23 +1,36 @@
 import { shallowMount } from "@vue/test-utils";
 import TotalSum from "@/components/TotalSum.vue";
 
-
 describe('TotalSum.vue', () => {
 
-	it('it should display "total sum" when component mounted', () => {
-		// arrange
+	it('it should display "Total sum" when component mounted', () => {
+        // arrange
+        const wrapper = shallowMount(TotalSum)
+		const expected = 'Total sum'
 
 		// act
-		
-		// assert
+        const actual = wrapper.find("h1").text()
+
+        // assert
+        expect(actual).toBe(expected)
 	})
 
     it('it should display total sum value 0 if no products in cart', () => {
-		// arrange
+        // arrange
+        const expectedCartOrders = 0
+        const expectedTotalSum = 0
 
-		// act
-		
-		// assert
+        const wrapper = shallowMount(TotalSum)
+        wrapper.setData({ totalSum: 0,        
+        })
+
+        // act
+        const actualCartOrders = wrapper.vm.cartOrders.length
+        const actualTotalSum = wrapper.vm.totalSum
+
+        // assert
+        expect(actualCartOrders).toBe(expectedCartOrders)
+        expect(actualTotalSum).toBe(expectedTotalSum)
 	})
 
     it('it should display the correct total sum if there is products in cart', () => {
