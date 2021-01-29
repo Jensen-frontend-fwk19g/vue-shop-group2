954c4bc8b8681b672af81cb5c2b2ceb69c31564f
jensengbg-sanel-jasarevic <55239936+jensengbg-sanel-jasarevic@users.noreply.github.com>
2021-01-29 20:41:15 +0100

    Added totalsum component & testcase file

---
+++ src/components/TotalSum.vue
@@ -0,0 +1,16 @@
+<template>
+
+
+</template>
+
+<script>
+export default {
+    name: "TotalSum",
+
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ tests/unit/TotalSum.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
+import TotalSum from "@/components/TotalSum.vue";
+
+
+describe('TotalSum.vue', () => {
+
+	it('', () => {
+		// arrange
+
+		// act
+		
+		// assert
+	})
+
+
+	
+})
+
+// 
