bbe814ffd46fafdc896f7bd7f6f7d8dfe16c5476
Grosse Riese <plmman2010@gmail.com>
2021-01-30 09:43:17 +0100

    update cart
    btn in the cart done

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,12 +1,46 @@
 <template>
   <aside id="cart">
-    <div class="cart-icon" ><span class="item-in-cart-cls">{{itemsInCart}} </span> </div>
-      <section class="cart-items" v-for="(item, index) in cart" :key="index" :id="item.id">
-        <p>{{item.name}}</p>
-        <p>{{item.price}}</p>
-        <button @click="removeItem(item)" :id="item.id" class="deleteBtn">&#10007;
-        </button>
-      </section>
+    <div class="cart-icon">
+      <span class="item-in-cart-cls">{{itemsInCart}} </span> 
+    </div>
+      <table id="customers" >
+              <thead>
+                <tr>
+                  <th style="width:60%;">Pets-name</th>
+                  <th>Price</th>
+                  <th>Pet-counts</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody >
+                <tr class="item-row">
+                  <td> Molly </td>
+                  <td> 1200 </td>
+                  <td >
+                    <small>
+                      <span> 1 </span>
+                    </small>
+                  </td>
+                  <td>
+                    <small>
+                      <button @click="removeItem(row)" class="deleteBtn">&#10007;</button>
+                    </small>
+                  </td>
+                </tr>
+                
+              </tbody>
+              
+              <tfoot>
+                <tr>
+                  <td style="width:60%;"><hr><br><b>Sum</b></td>
+                  <td><hr><br><b> totalCost </b> {{1200}}:-</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tfoot>
+            </table>
+
+
   </aside>
 </template>
 
@@ -17,23 +51,25 @@ export default {
   },
     data(){
     return{
-      cart: [ {
-          id: '1',
-          src:"https://picsum.photos/1024/480/?image=15",
-          name: 'Toy -1EC',
-          description: 'Lorem Ipsum is not simply random text.',
-          price: 399,
-          quantity:1
-        },
+      cart: [ 
         {
-          id: '2',
-          src:"https://picsum.photos/1024/480/?image=60",
-          name: 'PC-Toy -CC2719',
-          description: 'There are many variations of passages of Lorem Ipsum available',
-          price: 1995,
-          quantity:3
+            "name":"Molly",
+            "sex":"female",
+            "breed":"briard",
+            "img":"https://images.dog.ceo/breeds/briard/n02105251_6840.jpg",
+            "present":false,
+            "age":4,
+            "chipNumber":"IEH455006",
+              "owner":{
+                "name":"Wilmer",
+                "lastName":"Svensson",
+                "phoneNumber":"0769239356"
+                },
+            quantity:1,
+            price:399,
         }
-        ],
+      ],
+    
     }
   },
   computed: {
@@ -42,11 +78,27 @@ export default {
     },
   },
   methods: {
-  
-    removeItem(item){
-      let index= this.cart.indexOf(item);
-      this.cart.splice(index, 1);
+    removeItem(){
+    
     },
   }
 }
 </script>
+<style scoped>
+  #customers{
+    margin-top:3rem;
+  }
+  .item-in-cart-cls{
+    background-color: rgba(210, 254, 223, 0.593);
+    border:1px solid rgba(0, 128, 0, 0.635);
+    border-radius: 5px ;
+    padding:3px;
+  }
+  .deleteBtn{
+    background-color: rgba(238, 84, 84, 0.703);
+    color: rgb(128, 2, 2);
+    border:none;
+    border-radius: 3px;
+  }
+
+</style>
--- tests/unit/Cart.spec.js
+++ tests/unit/Cart.spec.js
@@ -3,28 +3,38 @@ import Cart from '@/components/Cart.vue'
 
 describe('Cart.vue', () => {
 //As a customer I want to be able to delete a product in for the cart
-  it('should display item/items in the cart to get delete Btn ', () => {
-
-    const expected = '2';
-    const componentWrapper = shallowMount(Cart);
-    const spanWrapper = componentWrapper.find('.item-in-cart-cls');
-    const actual = spanWrapper.text();
-    expect(actual).toBe(expected);
-
-  });
-
-  it('should contains the delete Button:s', () => {
-      const wrapper = shallowMount(Cart); 
-      const actual = wrapper.findAll('.deleteBtn');
-      expect(actual).toBeTruthy();
-    
-  });
-
-  it('should clicked to delete Item', async() => {
+  it('should display Cart length', () => {
     const wrapper = shallowMount(Cart);
-    await wrapper.find('.deleteBtn').trigger('removeItem');
-    expect(wrapper.vm.removeItem).toBeTruthy();
+    const expected = wrapper.vm.cart.length;
+    const actual = wrapper.find('.item-in-cart-cls').text();
+    expect(parseInt(actual)).toBe(expected);
   });
+
+  it('should show Item/s', async() => {
+    const wrapper = shallowMount(Cart);
+    const actual = wrapper.find('.item-row').exists();
+		expect(actual).toBe(true);
+  });
+
+  it("should be visible inside the cart ", () => {
+    const wrapper = shallowMount(Cart);
+    const expected = '<button class="deleteBtn">✗</button>';
+    const actualI = wrapper.find(".deleteBtn").html();
+    expect(actualI).toBe(expected);
+  });
+    //doing the same as above
+  it('should contains delete button inside the cart', () => {
+      const wrapper = shallowMount(Cart); 
+      const actual = wrapper.find('.deleteBtn');
+      expect(actual).toBeTruthy();
+  });
+
+  it('should contains [✗] as a text inside the btn', () => {
+    const wrapper = shallowMount(Cart); 
+    const actual = wrapper.find('.deleteBtn').text();
+    const expected = '✗';
+    expect(actual).toBe(expected);
+});
 
 
 })
