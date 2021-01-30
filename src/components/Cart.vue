9c45f870542c9c3732188daf97fb9df1bc161a4c
Grosse Riese <plmman2010@gmail.com>
2021-01-30 22:39:33 +0100

    more usecases to the cart, done

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -4,43 +4,46 @@
       <span class="item-in-cart-cls">{{itemsInCart}} </span> 
     </div>
       <table id="customers" >
-              <thead>
+        <thead>
                 <tr>
-                  <th style="width:60%;">Pets-name</th>
+                  <th style="width:20%;">Pets-name</th>
+                  <th>Description</th>
                   <th>Price</th>
-                  <th>Pet-counts</th>
+                  <th style="width:15%;">Qnty</th>
                   <th></th>
                 </tr>
-              </thead>
-              <tbody >
-                <tr class="item-row">
-                  <td> Molly </td>
-                  <td> 1200 </td>
+        </thead>
+        <tbody >
+                <tr class="item-row" v-if="showItem===true" :id="cart[0].chipNumber">
+                  <td> {{cart[0].name }} </td>
+                  <td> <small class="desc-item">Dog name is Molly, breed is briard...etc</small></td>
+                  <td class="item-price"> {{cart[0].price }} </td>
                   <td >
-                    <small>
-                      <span> 1 </span>
+                    <small class="item-qnty">
+                      <button @click="increaseQnt()"  class="arrow-up-btn"> + </button> 
+                      <span id="item-counts"> {{defaultQuantity}} </span>
+                      <button @click="decreaseQnt()"  class="arrow-down-btn"> - </button> 
                     </small>
                   </td>
                   <td>
                     <small>
-                      <button @click="removeItem(row)" class="deleteBtn">&#10007;</button>
+                      <button @click="removeItem()" class="deleteBtn">&#10007;</button>
                     </small>
                   </td>
                 </tr>
+                <tr v-else></tr>
                 
-              </tbody>
-              
-              <tfoot>
+        </tbody>
+        <tfoot>
                 <tr>
-                  <td style="width:60%;"><hr><br><b>Sum</b></td>
-                  <td><hr><br><b> totalCost </b> {{1200}}:-</td>
+                  <td style="width:30%;"><br><b>Sum</b></td>
+                  <td></td>
+                  <td v-if="showItem===true" class="item-total-cost"> <br> <b> {{totalCost}}:- </b></td> <td v-else class="item-total-cost">0</td>
                   <td></td>
                   <td></td>
                 </tr>
-              </tfoot>
-            </table>
-
-
+        </tfoot>
+      </table>
   </aside>
 </template>
 
@@ -48,9 +51,12 @@
 export default {
   name: 'Cart',
   props: {
+    //dogg:Object,
   },
     data(){
     return{
+      defaultQuantity:1,
+      showItem:true,
       cart: [ 
         {
             "name":"Molly",
@@ -74,17 +80,38 @@ export default {
   },
   computed: {
     itemsInCart(){
+      if(this.showItem){
         return this.cart.length;
+      }else{
+        return 0;
+      }
+        
     },
+      totalCost(){
+      return (this.cart[0].price * this.defaultQuantity);
+    }
   },
   methods: {
+    increaseQnt(){
+      this.defaultQuantity++;
+    },
+    decreaseQnt(){
+      this.defaultQuantity--;
+        if (this.defaultQuantity === 0) {
+          this.defaultQuantity =1;
+        }
+    },
     removeItem(){
-    
+      this.showItem =false;
+      
     },
   }
 }
 </script>
 <style scoped>
+#cart{
+  display: flex;
+}
   #customers{
     margin-top:3rem;
   }
@@ -93,12 +120,40 @@ export default {
     border:1px solid rgba(0, 128, 0, 0.635);
     border-radius: 5px ;
     padding:3px;
+    text-align: center;
   }
   .deleteBtn{
-    background-color: rgba(238, 84, 84, 0.703);
-    color: rgb(128, 2, 2);
+    background-color: rgba(254, 2, 2, 0.703);
+    color: rgb(239, 233, 233);
     border:none;
     border-radius: 3px;
+    cursor:pointer;
   }
-
+  .item-qnty{
+    border:1px solid black;
+    width:auto;
+  }
+  #item-counts{
+    margin:5px;
+  }
+  .arrow-up-btn{
+    background-color: rgba(84, 238, 107, 0.703);
+    color: rgb(15, 34, 19);
+    border:none;
+    border-radius: 2px;
+    cursor:pointer;
+  }
+  .arrow-down-btn{
+    background-color: rgba(238, 84, 84, 0.703);
+    color: rgb(34, 15, 15);
+    border:none;
+    border-radius: 2px;
+    cursor:pointer;
+  }
+  tfoot{
+    background-color: rgb(172, 234, 224);
+  }
+  thead{
+    background-color: rgba(138, 179, 172, 0.765);
+  }
 </style>
--- tests/unit/Cart.spec.js
+++ tests/unit/Cart.spec.js
@@ -2,39 +2,104 @@ import { shallowMount} from '@vue/test-utils'
 import Cart from '@/components/Cart.vue'
 
 describe('Cart.vue', () => {
-//As a customer I want to be able to delete a product in for the cart
+  /**
+   * As a customer I want to be able to delete a product in for the cart
+   * ============================================================================================
+   */
   it('should display Cart length', () => {
-    const wrapper = shallowMount(Cart);
-    const expected = wrapper.vm.cart.length;
-    const actual = wrapper.find('.item-in-cart-cls').text();
+    const wrapper   = shallowMount(Cart);
+    const expected  = wrapper.vm.cart.length;
+    const actual    = wrapper.find('.item-in-cart-cls').text();
     expect(parseInt(actual)).toBe(expected);
   });
 
-  it('should show Item/s', async() => {
-    const wrapper = shallowMount(Cart);
-    const actual = wrapper.find('.item-row').exists();
-		expect(actual).toBe(true);
-  });
-
-  it("should be visible inside the cart ", () => {
-    const wrapper = shallowMount(Cart);
-    const expected = '<button class="deleteBtn">✗</button>';
-    const actualI = wrapper.find(".deleteBtn").html();
+  it("should the btn be visible inside the cart ", () => {
+    const wrapper   = shallowMount(Cart);
+    const expected  = '<button class="deleteBtn">✗</button>';
+    const actualI   = wrapper.find(".deleteBtn").html();
     expect(actualI).toBe(expected);
   });
-    //doing the same as above
+
   it('should contains delete button inside the cart', () => {
-      const wrapper = shallowMount(Cart); 
-      const actual = wrapper.find('.deleteBtn');
-      expect(actual).toBeTruthy();
+    const wrapper   = shallowMount(Cart); 
+    const actual    = wrapper.find('.deleteBtn');
+    expect(actual).toBeTruthy();
   });
 
   it('should contains [✗] as a text inside the btn', () => {
-    const wrapper = shallowMount(Cart); 
-    const actual = wrapper.find('.deleteBtn').text();
-    const expected = '✗';
+    const wrapper   = shallowMount(Cart); 
+    const actual    = wrapper.find('.deleteBtn').text();
+    const expected  = '✗';
     expect(actual).toBe(expected);
-});
+    console.log('✗: = ', actual);
+  });
 
+  /**
+   * As a customer I would like to see the cart page with all the items the I want to buy.
+   * ==============================================================================================  
+   */
+  
+  it('should contain the item of the chosen-dog ', () => {
+    const wrapper   = shallowMount(Cart);
+    const actual    = wrapper.find('.item-row').exists();
+    expect(actual).toBe(true);
+  });
+
+  it('should has the same chip-number/id of the item in the cart & the displayed item', () => {
+    const wrapper   = shallowMount(Cart);
+    const actual    = wrapper.find('tr.item-row').element.getAttribute('id');
+    const expected  = wrapper.vm.cart[0].chipNumber;
+    expect(actual).toBe(expected);
+  });
+
+  it('should display the description of the item as the same to every item in the cart', () => {
+    const wrapper   = shallowMount(Cart);
+		const expected  = `Dog name is ${wrapper.vm.cart[0].name}, breed is ${wrapper.vm.cart[0].breed}...etc`;
+		const actual    = wrapper.find('.desc-item').text();
+		expect(actual).toMatch(expected);
+	})
+
+  it('should display, item-quantity = 1 as a default quantity when mounted', () => {
+    const expected    = '1';
+    const wrapper     = shallowMount(Cart);
+    const spanWrapper = wrapper.find('#item-counts');
+    const actual      = spanWrapper.text();
+    expect(actual).toBe(expected);
+  });
+
+  it('should increase item-quantity by 1 when mounted',async () => {
+    const expected       = (1+1) +'';
+    const wrapper        = shallowMount(Cart);
+    const increaseButton = wrapper.find('.arrow-up-btn');
+    await increaseButton.trigger('click');
+    const actual         = wrapper.find('#item-counts').text();
+    expect(actual).toBe(expected);
+  });
+
+  it('should decrease item-quantity by 1 when mounted', async() => {
+    const expected       = (1+3-2)+'';
+    const wrapper        = shallowMount(Cart);
+    const increaseButton = wrapper.find('.arrow-up-btn');
+    const decreaseButton = wrapper.find('.arrow-down-btn');
+    await increaseButton.trigger('click');//+
+    await decreaseButton.trigger('click');//-
+    await increaseButton.trigger('click');//+
+    await increaseButton.trigger('click');//+
+    await decreaseButton.trigger('click');//-
+    const actual         = wrapper.find('#item-counts').text();
+    expect(actual).toBe(expected);
+  });
+
+  it('should contain the item-price', () => {
+    const wrapper   = shallowMount(Cart);
+    const actual    = wrapper.find('.item-price').exists();
+    expect(actual).toBe(true);
+  });
+
+  it('should contain the total amount.', () => {
+    const wrapper   = shallowMount(Cart);
+    const actual    = wrapper.find('.item-total-cost').exists();
+    expect(actual).toBe(true);
+  });
 
 })
