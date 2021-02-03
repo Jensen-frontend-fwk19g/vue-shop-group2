7c746b117b1efd1d7c66c6ba748025035ac05bc7
Markus Bjerkhede <markus.bjerkhede@gmail.com>
2021-02-04 00:17:45 +0100

    add aplication vue and test, WIP

---
+++ tests/unit/ApplicationForm.spec.js
@@ -0,0 +1,40 @@
+import { shallowMount } from "@vue/test-utils";
+import ApplicationForm from "@/components/ApplicationForm.vue";
+
+
+describe("ApplicationForm.vue", () => {
+  it("should be hidden untill button to fill out the survey is clicked", () => {
+    //arrange
+    });
+
+    //act
+    
+    //assert
+    
+  });
+  it("should appear when the button to fill out the survey is clicked", () => {
+    //arrange
+    });
+
+    //act
+    
+    //assert
+    
+  it("should be able to enter values into input field", () => {
+    //arrange
+    });
+
+    //act
+    
+    //assert
+    
+
+  it("", () => {
+    //arrange
+    });
+
+    //act
+    
+    //assert
+    
+ 
