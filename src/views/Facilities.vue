ae501430389d908aaf84c90c19805cc53a65db2b
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-02-03 10:46:20 +0100

    facilities page work on progress

---
+++ src/views/Facilities.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainFacilities />
+</template>
+
+<script>
+import MainFacilities from "@/components/MainFacilities.vue";
+export default {
+  name: "Facilities",
+  components: {
+    MainFacilities,
+  },
+};
+</script>
+
+<style scoped></style>>
